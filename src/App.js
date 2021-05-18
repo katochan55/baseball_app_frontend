f5683605909741c8c18be90c065064b5c09227d1
kodai-kato <greatvoyage0122@gmail.com>
2021-05-18 22:34:42 +0900

    Display by league

--- src/App.js
+++ src/App.js
@@ -15,21 +15,41 @@ function useApp() {
     });
   }, []);
 
-  return { teams };
+  const centralTeams = teams.filter((team) => team.league === "central");
+  const pacificTeams = teams.filter((team) => team.league === "pacific");
+
+  return { centralTeams, pacificTeams };
 }
 
 function App() {
-  const { teams } = useApp();
+  const { centralTeams, pacificTeams } = useApp();
   return (
-    <ul>
-      {teams.map((team) => {
-        return (
-          <li key={team.id}>
-            {team.name}, {team.founded_year}
-          </li>
-        );
-      })}
-    </ul>
+    <div className="container mt-5">
+      <section>
+        <h2>セ・リーグ</h2>
+        <ul>
+          {centralTeams.map((team) => {
+            return (
+              <li key={team.id}>
+                {team.name}, {team.founded_year}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section className="mt-4">
+        <h2>パ・リーグ</h2>
+        <ul>
+          {pacificTeams.map((team) => {
+            return (
+              <li key={team.id}>
+                {team.name}, {team.founded_year}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
   );
 }
 
