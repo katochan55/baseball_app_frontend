f3740cad2d060b7b6e0d4ce7c2fa0c54cf06f87f
kodai-kato <greatvoyage0122@gmail.com>
2021-06-09 21:59:49 +0900

    Sort by Rank

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import { useState, useEffect } from "react";
 import { axios } from "./setup/axios";
+import camelize from "camelize";
 
 async function getTeams() {
   return await axios.get("/api/teams");
+}
+
+function ExtractByYear(teams, year) {
+  return teams.map((team) => {
+    const result = team.results.find((el) => el.year === year);
+    return {
+      ...team,
+      result: result,
+    };
+  });
+}
+
+function SortByRank(teams) {
+  return teams.sort((a, b) => a.result.rank - b.result.rank);
 }
 
 function useApp() {
@@ -10,7 +25,7 @@ function useApp() {
 
   useEffect(() => {
     getTeams().then((res) => {
-      const data = res.data.data;
+      const data = camelize(res.data.data);
       setTeams(data);
     });
   }, []);
@@ -18,34 +33,37 @@ function useApp() {
   const centralTeams = teams.filter((team) => team.league === "central");
   const pacificTeams = teams.filter((team) => team.league === "pacific");
 
-  return { centralTeams, pacificTeams };
+  const sortedCentralTeams = SortByRank(ExtractByYear(centralTeams, 2020));
+  const sortedPacificTeams = SortByRank(ExtractByYear(pacificTeams, 2020));
+
+  return { sortedCentralTeams, sortedPacificTeams };
+}
+
+function Team({ team }) {
+  return (
+    <li>
+      {team.name}, {team.foundedYear}, {team.result.rank}
+    </li>
+  );
 }
 
 function App() {
-  const { centralTeams, pacificTeams } = useApp();
+  const { sortedCentralTeams, sortedPacificTeams } = useApp();
   return (
     <div className="container mt-5">
       <section>
         <h2>セ・リーグ</h2>
         <ul>
-          {centralTeams.map((team) => {
-            return (
-              <li key={team.id}>
-                {team.name}, {team.founded_year}
-              </li>
-            );
+          {sortedCentralTeams.map((team) => {
+            return <Team team={team} key={`c-${team.id}`} />;
           })}
         </ul>
       </section>
       <section className="mt-4">
         <h2>パ・リーグ</h2>
         <ul>
-          {pacificTeams.map((team) => {
-            return (
-              <li key={team.id}>
-                {team.name}, {team.founded_year}
-              </li>
-            );
+          {sortedPacificTeams.map((team) => {
+            return <Team team={team} key={`p-${team.id}`} />;
           })}
         </ul>
       </section>
